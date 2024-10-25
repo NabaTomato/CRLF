2e45c68e5ecd8d9a44295f552b49b3d86a7ebeeb
Hanyu Yang <hanyuyang@Hanyu-Yang.local>
2024-10-24 17:17:00 -0700

    update

--- bash_script/step01_d_CRLF_catMultiLaneData_(incomplete).sh
+++ bash_script/step01_d_CRLF_catMultiLaneData_(incomplete).sh
@@ -9,70 +9,44 @@
 #$ -N step01_d_CRLF_catMultiLaneData
 
 # Version: v1
-# Usage: qsub step01_d_CRLF_catMultiLaneData_(incomplete).sh
-# Description: Concatenate raw data originating from multiple lanes of sequencing
+# Usage: qsub step01_d_CRLF_catMultiLaneData.sh <sample_id_1> <sample_id_2> ...
+# Description: Concatenate raw data originating from multiple lanes of sequencing for multiple samples
 # Author: Hanyu Yang (yhy020321@g.ucla.edu)
 # Date: Oct 24 2024
-# References: 
+# References:
 # https://knowledge.illumina.com/software/cloud-software/software-cloud-software-reference_material-list/000002035
 
 ## Setup workspace
 
 set -xeo pipefail
 
-## Define variables 
+## Define variables
 
 WORKDIR=/u/home/y/yhanyu/project-klohmuel/CRLF_raw_data
 
-## Main 
+## Main
 
-echo -e "[$(date "+%Y-%m-%d %T")] JOB ID ${JOB_ID}; Starting to cat multilane data" 
+echo -e "[$(date "+%Y-%m-%d %T")] JOB ID ${JOB_ID}; Starting to cat multi lane data for samples: $*"
 cd ${WORKDIR}
 
-# loop over individuals and concatenate the raw data
+# Loop over each sample ID
+for id in "$@"; do
+  echo -e "[$(date "+%Y-%m-%d %T")] Processing sample ${id}"
 
-echo -e "[$(date "+%Y-%m-%d %T")] JOB ID ${JOB_ID}; Forward read T4B005"
+  # forward reads (R1)
+  cat "${id}"_R1*.fq.gz > "${id}"_R1.fq.gz
+  # reverse reads (R2)
+  cat "${id}"_R2*.fq.gz > "${id}"_R2.fq.gz
 
-cat T4B005_ZKDN230030242-1A_HVVV3DSX7_L4_1.fq.gz T4B005_ZKDN230030242-1A_HVTJTDSX7_L4_1.fq.gz > T4B005_cat_R1.fq.gz 
-
-exitVal=${?}
-if [ ${exitVal} -ne 0 ]; then
-    echo -e "[$(date "+%Y-%m-%d %T")] FAIL" 
-    exit 1
-fi
-
-echo -e "[$(date "+%Y-%m-%d %T")] JOB ID ${JOB_ID}; Done with forward read T4B005"
-
-echo -e "[$(date "+%Y-%m-%d %T")] JOB ID ${JOB_ID}; Reverse read T4B005"
-
-cat T4B005_ZKDN230030242-1A_HVVV3DSX7_L4_2.fq.gz T4B005_ZKDN230030242-1A_HVTJTDSX7_L4_2.fq.gz > T4B005_cat_R2.fq.gz
-
-exitVal=${?}
-if [ ${exitVal} -ne 0 ]; then
-    echo -e "[$(date "+%Y-%m-%d %T")] FAIL" 
-    exit 1
-fi
-
-echo -e "[$(date "+%Y-%m-%d %T")] JOB ID ${JOB_ID}; Done with reverse read T4B005"
-
-# T3B092
-
-echo -e "[$(date "+%Y-%m-%d %T")] JOB ID ${JOB_ID}; T3B092"
-
-inds=(T3B092)
-for i in "${inds[@]}"; do
- cat ${i}*_R1* > ${i}_S17_L001_R1_001.fq.gz 
- cat ${i}*_R2* > ${i}_S17_L001_R2_001.fq.gz
+  exitVal=${?}
+  if [ ${exitVal} -ne 0 ]; then
+      echo -e "[$(date "+%Y-%m-%d %T")] FAIL for sample ${id}"
+      exit 1
+  fi
 done
 
-exitVal=${?}
-if [ ${exitVal} -ne 0 ]; then
-    echo -e "[$(date "+%Y-%m-%d %T")] FAIL"
-    exit 1
-fi
+echo -e "[$(date "+%Y-%m-%d %T")] JOB ID ${JOB_ID}; Done processing all samples"
 
-echo -e "[$(date "+%Y-%m-%d %T")] JOB ID ${JOB_ID}; Done with T3B092"
+## Cleanup
 
-## Cleanup ##
-
-echo -e "[$(date "+%Y-%m-%d %T")] JOB ID ${JOB_ID} Done concatenating multilane data"
+echo -e "[$(date "+%Y-%m-%d %T")] JOB ID ${JOB_ID} Done concatenating multi lane data for all samples"
